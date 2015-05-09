bf60f5230d612dbd00c271e0889191e9f25189cf
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-05-09 11:56:51 +1000

    Initial Commit

---
+++ SwiftStatisticsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftStatisticsDemo
+//
+//  Created by Evgenii Neumerzhitckii on 9/05/2015.
+//  Copyright (c) 2015 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
