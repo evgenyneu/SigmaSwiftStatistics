efd13204c68f507f9e786ef0daa3fe47e8bca636
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-07-25 16:47:27 +1000

    Add Mac target

---
+++ SigmaSwiftStatistics-Mac/SigmaSwiftStatistics-Mac.h
@@ -0,0 +1,19 @@
+//
+//  SigmaSwiftStatistics-Mac.h
+//  SigmaSwiftStatistics-Mac
+//
+//  Created by Evgenii on 25/07/2015.
+//  Copyright (c) 2015 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SigmaSwiftStatistics-Mac.
+FOUNDATION_EXPORT double SigmaSwiftStatistics_MacVersionNumber;
+
+//! Project version string for SigmaSwiftStatistics-Mac.
+FOUNDATION_EXPORT const unsigned char SigmaSwiftStatistics_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SigmaSwiftStatistics_Mac/PublicHeader.h>
+
+
