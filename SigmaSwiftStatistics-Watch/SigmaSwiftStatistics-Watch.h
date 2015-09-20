fa2b7a9f257fe40767ac384a8824d093e7fb4c5f
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-09-20 16:25:10 +1000

    Add target for Watch

---
+++ SigmaSwiftStatistics-Watch/SigmaSwiftStatistics-Watch.h
@@ -0,0 +1,19 @@
+//
+//  SigmaSwiftStatistics-Watch.h
+//  SigmaSwiftStatistics-Watch
+//
+//  Created by Evgenii on 20/09/2015.
+//  Copyright © 2015 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for SigmaSwiftStatistics-Watch.
+FOUNDATION_EXPORT double SigmaSwiftStatistics_WatchVersionNumber;
+
+//! Project version string for SigmaSwiftStatistics-Watch.
+FOUNDATION_EXPORT const unsigned char SigmaSwiftStatistics_WatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SigmaSwiftStatistics_Watch/PublicHeader.h>
+
+
