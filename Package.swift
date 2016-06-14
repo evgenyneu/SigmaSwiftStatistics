7ae33b2f73c7edf9364173341b19a1d9096f12c1
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-06-15 08:52:48 +1000

    Package

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "SigmaSwiftStatistics",
-    exclude: ["Graphics"],
+    exclude: ["Graphics", "SigmaSwiftStatistics-Mac", "SigmaSwiftStatistics-tvOS", "SigmaSwiftStatistics-Watch", "SigmaSwiftStatistics.xcodeproj", "SigmaSwiftStatisticsTests"],
     targets: [
         Target(
             name: "SigmaSwiftStatistics")
