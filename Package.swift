0e3277d9ae6f0fc53257ae26335ebf29ff25f25e
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-10-28 23:15:38 +1100

    Add normalDensity function

--- Package.swift
+++ Package.swift
@@ -2,5 +2,5 @@ import PackageDescription
 
 let package = Package(
     name: "SigmaSwiftStatistics",
-    exclude: ["Graphics", "SigmaSwiftStatistics-Mac", "SigmaSwiftStatistics-tvOS", "SigmaSwiftStatistics-Watch", "SigmaSwiftStatistics.xcodeproj", "SigmaSwiftStatisticsTests", "scripts", "SigmaSwiftStatistics"]
+    exclude: ["Graphics", "SigmaSwiftStatistics-Mac", "SigmaSwiftStatistics-tvOS", "SigmaSwiftStatistics-Watch", "SigmaSwiftStatistics.xcodeproj", "SigmaSwiftStatisticsTests", "scripts", "Distrib"]
 )
