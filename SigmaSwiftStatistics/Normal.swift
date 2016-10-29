61d3456f96ee32f8879f2400fef205832b884da9
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-10-29 17:03:51 +1100

    Fix comments

--- SigmaSwiftStatistics/Normal.swift
+++ SigmaSwiftStatistics/Normal.swift
@@ -77,7 +77,7 @@ public extension Sigma {
    
    - parameter σ: The standard deviation. Default: 1.
    
-   - returns: the quantile function for the normal distribution. Returns nil if σ is zero or negative. Returns nil if p is negative or greater than one. Returns (-Double.infinity) if p is zero. Returns Double.infinity if p is one.
+   - returns: The quantile function for the normal distribution. Returns nil if σ is zero or negative. Returns nil if p is negative or greater than one. Returns (-Double.infinity) if p is zero. Returns Double.infinity if p is one.
    
    
    Example:
@@ -146,7 +146,7 @@ public extension Sigma {
    
    - parameter σ: The standard deviation.
    
-   - returns: the quantile function for the normal distribution. Returns nil if σ is zero or negative. Returns nil if p is negative or greater than one. Returns (-Double.infinity) if p is zero. Returns Double.infinity if p is one.
+   - returns: The quantile function for the normal distribution. Returns nil if σ is zero or negative. Returns nil if p is negative or greater than one. Returns (-Double.infinity) if p is zero. Returns Double.infinity if p is one.
    
   */
   static func qnorm(p: Double, mu: Double, sigma: Double) -> Double? {
