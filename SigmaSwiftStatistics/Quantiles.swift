60d35be1829973e704f458102c44f98cc51e8942
Alan J. Salmoni <salmoni@gmail.com>
2016-12-25 20:25:45 +0000

    Quantile functions created (9 functions)
    This is the first commit of 9 functions to calculate quantiles. All
    work was taken from the Hyndman and Fan paper (1996) and a PDF of the
    paper can be accessed at
    https://www.amherst.edu/media/view/129116/original/Sample+Quantiles.pdf

---
+++ SigmaSwiftStatistics/Quantiles.swift
@@ -0,0 +1,114 @@
+//
+//  Quantiles.swift
+//
+//  Created by Alan James Salmoni on 21/12/2016.
+//  Copyright © 2016 Thought Into Design Ltd. All rights reserved.
+//
+
+import Foundation
+
+func QDef(data: [Double], k: Int, alpha: Double) -> Double {
+    /* Unsure if return is a double or an array? */
+    let qdef = ((1.0 - alpha)*data[k-1])+(alpha*data[k])
+    return qdef
+}
+
+func Q1(data: [Double], alpha: Double) -> Double {
+    let data = data.sorted(by: >)
+    let count = data.count
+    let k = Int((alpha * Double(count)) + 0.5)
+    let alpha = (alpha * Double(count)) - Double(k) + 0.5
+    let Q = QDef(data: data, k: k, alpha: alpha)
+    return Q
+}
+
+func Q2(data: [Double], alpha: Double) -> Double {
+    let data = data.sorted(by: >)
+    let count = data.count
+    let g = alpha * Double(count)
+    let k = Int(g)
+    if g == 0.0 {
+        let alpha = 0.5
+    }
+    else {
+        let alpha = 1.0
+    }
+    let Q = QDef(data: data, k: k, alpha: alpha)
+    return Q
+}
+
+func Q3(data: [Double], alpha: Double) -> Double {
+    let data = data.sorted(by: >)
+    let count = data.count
+    let m = -0.5
+    let k = Int((alpha * Double(count)) + m)
+    let g = (alpha * Double(count)) + m - Double(k)
+    var alpha = 1.0
+    /* if g == 0.0 && k.truncatingRemainder(dividingBy: 2.0) != 0.0 { */
+    if g == 0.0 && k % 2 != 0 {
+        alpha = 0.0
+    }
+    let Q = QDef(data: data, k: k, alpha: alpha)
+    return Q
+}
+
+func Q4(data: [Double], alpha: Double) -> Double {
+    let data = data.sorted(by: >)
+    let count = data.count
+    let m = 0.0
+    let k = Int((alpha * Double(count)) + m)
+    let alpha = (alpha * Double(count)) + m - Double(k)
+    let Q = QDef(data: data, k: k, alpha: alpha)
+    return Q
+}
+
+func Q5(data: [Double], alpha: Double) -> Double {
+    let data = data.sorted(by: >)
+    let count = data.count
+    let m = 0.5
+    let k = Int((alpha * Double(count)) + m)
+    let alpha = (alpha * Double(count)) + m - Double(k)
+    let Q = QDef(data: data, k: k, alpha: alpha)
+    return Q
+}
+
+func Q6(data: [Double], alpha: Double) -> Double {
+    let data = data.sorted(by: >)
+    let count = data.count
+    let m = alpha
+    let k = Int((alpha * Double(count)) + m)
+    let alpha = (alpha * Double(count)) + m - Double(k)
+    let Q = QDef(data: data, k: k, alpha: alpha)
+    return Q
+}
+
+func Q7(data: [Double], alpha: Double) -> Double {
+    let data = data.sorted(by: >)
+    let count = data.count
+    let m = 1.0 - alpha
+    let k = Int((alpha * Double(count)) + m)
+    let alpha = (alpha * Double(count)) + m - Double(k)
+    let Q = QDef(data: data, k: k, alpha: alpha)
+    return Q
+}
+
+func Q8(data: [Double], alpha: Double) -> Double {
+    let data = data.sorted(by: >)
+    let count = data.count
+    let m = (alpha + 1.0) / 3.0
+    let k = Int((alpha * Double(count)) + m)
+    let alpha = (alpha * Double(count)) + m - Double(k)
+    let Q = QDef(data: data, k: k, alpha: alpha)
+    return Q
+}
+
+func Q9(data: [Double], alpha: Double) -> Double {
+    let data = data.sorted(by: >)
+    let count = data.count
+    let m = (0.25 * alpha) + (3.0 / 8.0)
+    let k = Int((alpha * Double(count)) + m)
+    let alpha = (alpha * Double(count)) + m - Double(k)
+    let Q = QDef(data: data, k: k, alpha: alpha)
+    return Q
+}
+
