60d2d88d036490e85bf986a739dc5ad9af54bff2
Alan J. Salmoni <salmoni@gmail.com>
2017-01-03 17:16:42 +0000

    Minor changes
    1. Adjusted spacing to 2 spaces (was 4)
    2. Adjusted the routine to check the array being sent has content

--- SigmaSwiftStatistics/Moment.swift
+++ SigmaSwiftStatistics/Moment.swift
@@ -9,45 +9,45 @@
 import Foundation
 
 public extension Sigma {
-    /**
-     
-     Computes a specified moment based on a sample.
-     
-     https://en.wikipedia.org/wiki/Moment_(mathematics)
-     
-     - parameter values: Array of decimal numbers, dimension
-     - returns: Moment based on a sample. Returns nil when the array is empty or contains a single value.
-     
-     Formula:
-     
-     [XXXX]
-     
-     Where:
-     
-     m is the required dimension.
-     
-     n is the sample size.
-     
-     Example:
-     
-     Sigma.varianceSample([1, 12, 19.5, -5, 3, 8]) // 75.24166667
-     
-     */
+  /**
+   
+   Computes a specified moment based on a sample.
+   
+   https://en.wikipedia.org/wiki/Moment_(mathematics)
+   
+   - parameter values: Array of decimal numbers, dimension
+   - returns: Moment based on a sample. Returns nil when the array is empty or contains a single value.
+   
+   Formula:
+   
+   [XXXX]
+   
+   Where:
+   
+   m is the required dimension.
+   
+   n is the sample size.
+   
+   Example:
+   
+   Sigma.varianceSample([1, 12, 19.5, -5, 3, 8]) // 75.24166667
+   
+  */
 
-    public static func moment(_ values: [Double], m: Int ) -> Double? {
-        let average_val = average(values)
-        let count = Double(values.count)
-        var total: Double = 0
-        var delta: Double = 0
-        if values.count > 0 {
-            for value in values {
-                delta = pow((value - average_val!), Double(m))
-                total += delta
-            }
-            return total / count
-        }
-        else {
-            return nil
-        }
+  public static func moment(_ values: [Double], m: Int ) -> Double? {
+    let count = Double(values.count)
+    if values.count > 0 {
+      let average_val = average(values)
+      var total: Double = 0
+      var delta: Double = 0
+      for value in values {
+        delta = pow((value - average_val!), Double(m))
+        total += delta
+      }
+      return total / count
     }
+    else {
+      return nil
+    }
+  }
 }
