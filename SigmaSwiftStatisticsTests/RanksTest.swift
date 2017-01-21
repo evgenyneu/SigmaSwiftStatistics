ad192f0cb1099d4b4bcddcfd0b80e13b7762c5c3
Alan J. Salmoni <salmoni@gmail.com>
2017-01-21 20:17:17 +0000

    Changed unit test names
    Changed unit test names to make them more descriptive of what types of
    things they test

--- SigmaSwiftStatisticsTests/RanksTest.swift
+++ SigmaSwiftStatisticsTests/RanksTest.swift
@@ -74,22 +74,22 @@ class RanksTests: XCTestCase {
     XCTAssertEqual([50.5, 59.5, 53.0, 55.5, 53.0, 50.5, 53.0, 55.5, 57.5, 59.5, 57.5], result)
   }
   
-  func testRanksMin() {
+  func testRanksStartTiesMin() {
     let result = Sigma.rank([1.0 ,6.0, 2.0 ,3.0 ,2.0 ,1.0 ,2.0 ,3.0 ,5.0 ,6.0 ,5.0], start: 25, ties: "min")!
     XCTAssertEqual([25.0, 34.0, 27.0, 30.0, 27.0, 25.0, 27.0, 30.0, 32.0, 34.0, 32.0], result)
   }
   
-  func testRanksMax() {
+  func testRanksStartTiesMax() {
     let result = Sigma.rank([1.0 ,6.0, 2.0 ,3.0 ,2.0 ,1.0 ,2.0 ,3.0 ,5.0 ,6.0 ,5.0], start: -99, ties: "max")!
     XCTAssertEqual([-98.0, -89.0, -95.0, -93.0, -95.0, -98.0, -95.0, -93.0, -91.0, -89.0, -91.0], result)
   }
   
-  func testRanksFirst() {
+  func testRanksStartTiesFirst() {
     let result = Sigma.rank([1.0 ,6.0, 2.0 ,3.0 ,2.0 ,1.0 ,2.0 ,3.0 ,5.0 ,6.0 ,5.0], start: 0, ties: "first")!
     XCTAssertEqual([0.0, 9.0, 2.0, 5.0, 3.0, 1.0, 4.0, 6.0, 7.0, 10.0, 8.0], result)
   }
   
-  func testRanksLast() {
+  func testRanksStartTiesLast() {
     let result = Sigma.rank([1.0 ,6.0, 2.0 ,3.0 ,2.0 ,1.0 ,2.0 ,3.0 ,5.0 ,6.0 ,5.0], start: 2500, ties: "last")!
     XCTAssertEqual([2501.0, 2510.0, 2504.0, 2506.0, 2503.0, 2500.0, 2502.0, 2505.0, 2508.0, 2509.0, 2507.0], result)
   }
