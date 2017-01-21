99bf8124303a6473fdf6177367e9c3d519fc825f
Alan J. Salmoni <salmoni@gmail.com>
2017-01-21 19:36:01 +0000

    Adjusted indentation to 2 spaces

--- SigmaSwiftStatistics/CoefficientVariation.swift
+++ SigmaSwiftStatistics/CoefficientVariation.swift
@@ -8,38 +8,38 @@
 import Foundation
 
 public extension Sigma {
-    /**
-     
-     Computes coefficient of variation based on a sample.
-     
-     https://en.wikipedia.org/wiki/Coefficient_of_variation
-     
-     - parameter values: Array of decimal numbers.
-     - returns: Coefficient of variation of a sample. Returns nil when the array is empty or contains a single value.
-     
-     Formula:
-     
-     s = sqrt( Σ( (x - m)^2 ) / (n - 1) )
-     
-     Where:
-     
-     m is the sample mean.
-     
-     n is the sample size.
-     
-     Example:
-     
-     Sigma.coefficient_variation([1, 12, 19.5, -5, 3, 8]) // 1.3518226671899016
-     
-     */
-    public static func coefficient_variation(_ values: [Double]) -> Double? {
-        if values.count > 0 {
-            let sampleStdDev = Sigma.standardDeviationSample(values)
-            let average_val = average(values)
-            return sampleStdDev! / average_val!
-        }
-        else {
-            return nil
-        }
+  /**
+   
+   Computes coefficient of variation based on a sample.
+   
+   https://en.wikipedia.org/wiki/Coefficient_of_variation
+   
+   - parameter values: Array of decimal numbers.
+   - returns: Coefficient of variation of a sample. Returns nil when the array is empty or contains a single value.
+   
+   Formula:
+   
+   s = sqrt( Σ( (x - m)^2 ) / (n - 1) )
+   
+   Where:
+   
+   m is the sample mean.
+   
+   n is the sample size.
+   
+   Example:
+   
+   Sigma.coefficient_variation([1, 12, 19.5, -5, 3, 8]) // 1.3518226671899016
+   
+   */
+  public static func coefficient_variation(_ values: [Double]) -> Double? {
+    if values.count > 0 {
+      let sampleStdDev = Sigma.standardDeviationSample(values)
+      let average_val = average(values)
+      return sampleStdDev! / average_val!
     }
+    else {
+      return nil
+    }
+  }
 }
