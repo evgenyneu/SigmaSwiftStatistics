7e188bb7ad80ae8c5ed2eb9e04a85d83e5442f87
Alan J. Salmoni <salmoni@gmail.com>
2017-01-23 16:40:38 +0000

    Changed line spacing - unimportant

--- SigmaSwiftStatisticsTests/UniqueValuesTests.swift
+++ SigmaSwiftStatisticsTests/UniqueValuesTests.swift
@@ -8,17 +8,17 @@ class UniqueValuesTests: XCTestCase {
     let result = Sigma.uniqueValues(values: [])
     XCTAssertNil(result)
   }
-
+  
   func testArraySingle() {
     let result = Sigma.uniqueValues(values: [-99])
     XCTAssertEqual([-99], result!)
   }
-
+  
   func testArrayHomogenous() {
     let result = Sigma.uniqueValues(values: [55,55,55,55,55])
     XCTAssertEqual([55], result!)
   }
-
+  
   func testArrayPositive() {
     let result = Sigma.uniqueValues(values: [2,5,6,5,6,2,5])
     XCTAssertEqual([2,5,6], result!)
@@ -28,7 +28,7 @@ class UniqueValuesTests: XCTestCase {
     let result = Sigma.uniqueValues(values: [-4,-5,-4,-5,-99,-5])?.sorted(by: <)
     XCTAssertEqual([-99, -5,-4], result!)
   }
-
+  
   func testArrayMixed() {
     let result = Sigma.uniqueValues(values: [55,-55,10,-10,-999, 10, -10, -55, 55, -999, 55])?.sorted(by: <)
     XCTAssertEqual([-999, -55, -10, 10, 55], result!)
