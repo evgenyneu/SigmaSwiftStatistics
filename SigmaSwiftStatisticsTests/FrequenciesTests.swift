86095e6f2fc13943cb19cfff12dd88a1394a5f11
Alan J. Salmoni <salmoni@gmail.com>
2017-01-23 11:01:11 +0000

    Improved test code
    Removed all ‘var’ declarations and replaced with ‘let’ reducing code
    length and increasing clarity.

--- SigmaSwiftStatisticsTests/FrequenciesTests.swift
+++ SigmaSwiftStatisticsTests/FrequenciesTests.swift
@@ -15,9 +15,7 @@ class FrequenciesTests: XCTestCase {
 
   func testSingleArray() {
     if let result = Sigma.frequencies(values: [-99.999]) {
-      var unique_values: [Double]
-      var value_frequencies: [Int]
-      (unique_values, value_frequencies) = result
+      let (unique_values, value_frequencies) = result
       // (-99.999, [1])
       XCTAssertEqual([-99.999], unique_values)
       XCTAssertEqual([1], value_frequencies)
@@ -29,9 +27,7 @@ class FrequenciesTests: XCTestCase {
   
   func testHomogenousArray() {
     if let result = Sigma.frequencies(values: [-99.999, -99.999, -99.999, -99.999, -99.999, -99.999]) {
-      var unique_values: [Double]
-      var value_frequencies: [Int]
-      (unique_values, value_frequencies) = result
+      let (unique_values, value_frequencies) = result
       // (-99.999, [6])
       XCTAssertEqual([-99.999], unique_values)
       XCTAssertEqual([6], value_frequencies)
@@ -40,12 +36,10 @@ class FrequenciesTests: XCTestCase {
       XCTAssertNil(nil)
     }
   }
-  
+
   func testPositiveArray() {
     if let result = Sigma.frequencies(values: [6,2,9999,9999,2,79,6,6]) {
-      var unique_values: [Double]
-      var value_frequencies: [Int]
-      (unique_values, value_frequencies) = result
+      let (unique_values, value_frequencies) = result
       // (2,6,79,9999, [2,3,1,2])
       XCTAssertEqual([2,6,79,9999], unique_values)
       XCTAssertEqual([2,3,1,2], value_frequencies)
@@ -57,9 +51,7 @@ class FrequenciesTests: XCTestCase {
 
   func testNegativeArray() {
     if let result = Sigma.frequencies(values: [-99.999, -0.0001, -99.999, -99.999, -99.999, -99.999]) {
-      var unique_values: [Double]
-      var value_frequencies: [Int]
-      (unique_values, value_frequencies) = result
+      let (unique_values, value_frequencies) = result
       // (-99.999, [1])
       XCTAssertEqual([-99.999, -0.0001], unique_values)
       XCTAssertEqual([5,1], value_frequencies)
@@ -71,14 +63,10 @@ class FrequenciesTests: XCTestCase {
   
   func testMixedArray() {
     if let result = Sigma.frequencies(values: [-99.999, -99.999, -99.999, 99.999, -99.999, 99.999]) {
-      var unique_values: [Double]
-      var value_frequencies: [Int]
-      (unique_values, value_frequencies) = result
+      let (unique_values, value_frequencies) = result
       // (-99.999, [1])
       XCTAssertEqual([-99.999, 99.999], unique_values)
       XCTAssertEqual([4,2], value_frequencies)
     }
   }
-  
-    
 }
