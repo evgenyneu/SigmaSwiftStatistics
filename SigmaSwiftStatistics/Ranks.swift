3f34fe176d984167c2d85b151d357f2bc25a54e9
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2017-02-11 15:23:14 +1100

    Add ranks function

---
+++ SigmaSwiftStatistics/Ranks.swift
@@ -0,0 +1,89 @@
+//
+//  Ranks.swift
+//  SigmaSwiftStatistics
+//
+//  Created by Alan James Salmoni on 21/01/2017.
+//  Copyright © 2017 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+import Foundation
+
+public extension Sigma {
+  
+  /*
+   this ranks a vector (single dimensional array) of floats.
+   
+   Parameter values:
+   - Array of doubles to be ranked
+   - Start value for ranking (defaults to 1)
+   - How to deal with ties. Defaults to "mean"
+   - "mean" uses the arithmetic mean,
+   - "max" uses the maximum possible rank for all ties
+   - "min" uses the minimum rank for all ties
+   - "first" awards a descending rank starting so first occurence gets the highest rank down to the last
+   - "last" awards an ascending rank starting so the last occurance gets the highest rank down to the first
+   
+   Returns:
+   - Array of floats with the rank of each item
+   
+   Examples:
+   Sigma.rank([2,3,6,5,3]) // [1.0, 2.5, 5.0, 4.0, 2.5]
+   Sigma.rank([100,100,100,100], start: 10) // [11.5, 11.5, 11.5, 11.5]
+   Sigma.rank([100,100,100,100], ties: "max") // [1.0, 1.0, 1.0, 1.0]
+   Sigma.rank([100,100,100,100], ties: "min") // [4.0, 4.0, 4.0, 4.0]
+   Sigma.rank([100,100,100,100], ties: "first") // [1.0, 2.0, 3.0, 4.0]
+   Sigma.rank([100,100,100,100], ties: "last") // [4.0, 3.0, 2.0, 1.0]
+   
+   */
+  
+  public static func rank(_ values: [Double], start: Double = 1.0, ties: String = "mean") -> [Double] {
+    var rank: Double
+    
+    if ties == "mean" {
+      rank = start - 0.5
+    }
+    else if ties == "max" || ties == "min" || ties == "first" || ties == "last" {
+      rank = start - 1.0
+    }
+    else {
+      return []
+    }
+    
+    var increment: Double
+    var tiny_increment: Double
+    let frequencies =  Sigma.frequencies(values)
+    
+    var ranks = [Double](repeating: 0, count: values.count)
+    
+    for (value, frequency) in frequencies {
+      increment = Double(frequency)
+      tiny_increment = 1.0
+      
+      for idx in 0...(values.count - 1) {
+        if value == values[idx] {
+          if ties == "mean" {
+            ranks[idx] = rank + (increment / 2.0)
+          }
+          else if ties == "min" {
+            ranks[idx] = rank + 1
+          }
+          else if ties == "max" {
+            ranks[idx] = rank + increment
+          }
+          else if ties == "first" {
+            ranks[idx] = rank + tiny_increment
+            tiny_increment += 1
+          }
+          else if ties == "last" {
+            ranks[idx] = rank + increment - tiny_increment + 1.0
+            tiny_increment += 1
+          }
+        }
+      }
+      
+      rank += increment
+    }
+    
+    return ranks
+  }
+}
